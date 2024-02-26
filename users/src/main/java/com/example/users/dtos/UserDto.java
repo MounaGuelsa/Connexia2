b804e17b5fc5e0dbf6be8d92068edd8b98ea464c
ABDESSADEQMAKKIOUI <abdessadeq.elmakkioui20@gmail.com>
2024-02-26 15:39:57 +0100

    implement sevice users

---
+++ users/src/main/java/com/example/users/dtos/UserDto.java
@@ -0,0 +1,7 @@
+package com.example.users.dtos;
+
+/**
+ * DTO for {@link com.example.users.entities.User}
+ */
+public class UserDto {
+}
---
+++ users/src/main/java/com/example/users/repositories/UserRepository.java
@@ -0,0 +1,4 @@
+package com.example.users.repositories;
+
+public interface UserRepository extends org.springframework.data.jpa.repository.JpaRepository<com.example.users.entities.User, java.lang.Long> {
+}
