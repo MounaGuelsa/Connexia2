77ae9f5020e935b7ab4a57474c5710d81f0363c5
Yassin Ezaouibi <zouibiyassin03@gmail.com>
2024-02-27 09:12:19 +0100

    yassin is starting coding in his microservice

---
+++ spervice-publication-contenus/src/main/java/com/casablanca/SpervicePublicationContenusApplication.java
@@ -0,0 +1,13 @@
+package com.casablanca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpervicePublicationContenusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpervicePublicationContenusApplication.class, args);
+	}
+
+}
