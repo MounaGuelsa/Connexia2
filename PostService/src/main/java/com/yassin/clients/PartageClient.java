00007217e799c3e86901009c78c4a71b4b2c5391
YassinEzaouibi <zouibiyassin03@gmail.com>
2024-02-28 16:15:35 +0100

    je doit ajouter des modification demain InchaeAllah

--- PostService/src/main/java/com/yassin/clients/PartageClient.java
+++ PostService/src/main/java/com/yassin/clients/PartageClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-public interface partageClient {
+public interface PartageClient {
     @GetMapping("/{id}")
     public ResponseEntity<PartageDto> getPartageById(@PathVariable Long id);
 }
