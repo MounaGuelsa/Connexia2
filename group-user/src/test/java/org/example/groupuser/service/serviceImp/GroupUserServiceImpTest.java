9bf37446168542e063c915626fb3ee5aa320b3af
mariamL <laghfirimariam@gmail.com>
2024-02-29 14:44:06 +0100

    update group-user service

---
+++ group-user/src/test/java/org/example/groupuser/service/serviceImp/GroupUserServiceImpTest.java
@@ -0,0 +1,28 @@
+package org.example.groupuser.service.serviceImp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GroupUserServiceImpTest {
+
+    @Test
+    void showGroupUsers() {
+    }
+
+    @Test
+    void findGroupByUserId() {
+    }
+
+    @Test
+    void getGroupUserById() {
+    }
+
+    @Test
+    void addGroupUser() {
+    }
+
+    @Test
+    void deleteGroupUser() {
+    }
+}
